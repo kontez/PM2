7e337aa5d5073ea92db0851bb949c94325065a0d
Marvin Contessi <kontez@users.noreply.github.com>
2016-10-23 18:18:10 +0200

    indent fix

--- A1/src/umsatz/Rechnung.java
+++ A1/src/umsatz/Rechnung.java
@@ -3,37 +3,37 @@ package umsatz;
 import java.util.Arrays;
 
 public class Rechnung {
-	private int initialeKapazitaet = 5;
-	private int cursor;
-	private int rechnungsnummer;
-	private Position positionen[];
-	private int erweiternUm = 2;
-	
-	public Rechnung(int rechnungsnummer) {
-		this.rechnungsnummer = rechnungsnummer;
-		positionen = new Position[initialeKapazitaet];
-		cursor = 0;
-	}
-	
-	public Position[] getPositionen() {
-		return positionen;
+    private int initialeKapazitaet = 5;
+    private int cursor;
+    private int rechnungsnummer;
+    private Position positionen[];
+    private int erweiternUm = 2;
+    
+    public Rechnung(int rechnungsnummer) {
+        this.rechnungsnummer = rechnungsnummer;
+        positionen = new Position[initialeKapazitaet];
+        cursor = 0;
     }
-	
-	public void add(Position p){
-		if (cursor >= positionen.length) {
-			positionen = Arrays.copyOf(positionen, positionen.length + erweiternUm);
-		}
-		positionen[cursor] = p;
-		cursor++;	
-	}
-	
-	@Override
-	public String toString() {
-		return positionen[positionen.length] != null ? Arrays.toString(positionen) : 
-			Arrays.toString(Arrays.copyOf(positionen, cursor - 1));
-	}
-	
-	public GeldBetrag rechnungsSumme() {
+    
+    public Position[] getPositionen() {
+        return positionen;
+    }
+    
+    public void add(Position p){
+        if (cursor >= positionen.length) {
+            positionen = Arrays.copyOf(positionen, positionen.length + erweiternUm);
+        }
+        positionen[cursor] = p;
+        cursor++;   
+    }
+    
+    @Override
+    public String toString() {
+        return positionen[positionen.length] != null ? Arrays.toString(positionen) : 
+            Arrays.toString(Arrays.copyOf(positionen, cursor - 1));
+    }
+    
+    public GeldBetrag rechnungsSumme() {
         GeldBetrag total = new GeldBetrag();
         for( Position p : positionen ) {
             if (p != null) {
@@ -41,8 +41,8 @@ public class Rechnung {
             }
         }
         return total;
-	}
-	
+    }
+    
     public GeldBetrag summePosition(Position pos) {
         GeldBetrag gesamt = new GeldBetrag();
         for ( Position p : positionen ) {
@@ -52,5 +52,4 @@ public class Rechnung {
         }
         return gesamt;
     }
-
 }
